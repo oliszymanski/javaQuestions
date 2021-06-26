51f6a388830574526ec63fdb870ebd847809e577
Oliwier Szymański <oliwier.szymanski112@gmail.com>
2021-06-26 20:28:00 +0200

    initial commit

---
+++ src/sample/helloPackage/mainPackageClass.java
@@ -0,0 +1,6 @@
+package sample.helloPackage;
+
+public class mainPackageClass {
+	public String whoAmI = "Don't ask me again.. I'm your package";
+	public String requestCoco = "I will not give you Coco";
+}
---
+++ src/sample/newApp.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class newApp extends Application {
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+
+
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+}
