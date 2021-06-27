e348be8ce06ba1e723228e809d1e0ce17b4ba870
Oliwier Szymański <oliwier.szymanski112@gmail.com>
2021-06-27 22:47:59 +0200

    update main

--- out/production/askQuestions/sample/Main.java
+++ out/production/askQuestions/sample/Main.java
@@ -1,10 +1,14 @@
+// main package
 package sample;
 
+// imports
 import sample.helloPackage.mainPackageClass;
 import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
+
+        mainPackageClass MPC = new mainPackageClass();
 
     }
 }
