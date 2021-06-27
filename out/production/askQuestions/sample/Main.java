40f3519d4d75cfe901576d9449189bbe12edfb10
Oliwier Szymański <oliwier.szymanski112@gmail.com>
2021-06-27 22:56:16 +0200

    just ask some questions
    straight answer

--- out/production/askQuestions/sample/Main.java
+++ out/production/askQuestions/sample/Main.java
@@ -11,7 +11,14 @@ class Main{
         Scanner scanner = new Scanner(System.in);
         mainPackageClass MPC = new mainPackageClass();
 
-        
+        System.out.println("Who am I? \n"+MPC.whoAmI);
+        System.out.println("Can you give me Coco? \n"+MPC.requestCoco);
+
+        System.out.print("Please write your desired username: ");	//note: used 'print' to show the input in the same line
+        String username = scanner.next();		// input place
+
+        System.out.println("Account with username "+username+" created!");		// sort of a "check"
+        System.out.println("Hello "+username+"!");
 
     }
 }
