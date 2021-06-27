a2b7f8df42dd3b5fa0c5e448d584a1192768a1eb
Oliwier Szymański <oliwier.szymanski112@gmail.com>
2021-06-27 22:54:04 +0200

    creating a scanner object
    just forgot Scanner(System.in)

--- out/production/askQuestions/sample/Main.java
+++ out/production/askQuestions/sample/Main.java
@@ -8,7 +8,10 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         mainPackageClass MPC = new mainPackageClass();
+
+        
 
     }
 }
