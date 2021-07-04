6b6ea32961fe6c8e7b9ad7c7d297b1ca973f8051
Oliwier Szymański <oliwier.szymanski112@gmail.com>
2021-07-05 00:15:01 +0200

    test method
    Testing getUserStatus() method in two classes

--- Users/User_00.java
+++ Users/User_00.java
@@ -1,7 +1,12 @@
 package com.company.allEnums.Users;
 
+
 public class User_00 {
 	public static void main(String[] args) {
 
+		UserStatus myNewUser = new UserStatus();
+		UserStatus.Status myUserStatus = UserStatus.Status.ACTIVE;
+
+		myNewUser.getUserStatus(myUserStatus);
 	}
 }
--- Users/User_01.java
+++ Users/User_01.java
@@ -1,7 +1,16 @@
 package com.company.allEnums.Users;
 
+
 public class User_01 {
+
 	public static void main(String[] args) {
-		
+
+		UserStatus myUser = new UserStatus();		// class object
+		UserStatus.Status myUserStatus = UserStatus.Status.LAST_ACTIVE;
+
+
+		myUser.getUserStatus(myUserStatus); 	// getting the user status
+
 	}
+
 }
